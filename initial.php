e44b42843ba78fe4f923c579f8c568b945614018
samlummis <sam.lummis@gmail.com>
2010-12-03 09:51:36 +1300

    Test commit

--- initial.php
+++ initial.php
@@ -1,19 +1,17 @@
 <!DOCTYPE HTML>
 
 <html>
-	<div class="test">
 	<head>
 	<title>Test</title>
 		<LINK href="testcss.css" rel="stylesheet" type="text/css">
 		<script type="text/javascript" src="testjs.js"></script>
 	</head>	
 	<body>
-		<div id="bg"><img src="Assets/testBackground.png" width="100%" height="100%" alt=""></div>
 		<div id="content">
-		<video src="http://ftp.gnu.org/video/Stephen_Fry-Happy_Birthday_GNU-hq_600px_780kbit.ogv" controls="control">
+		<video src="http://ftp.gnu.org/video/Stephen_Fry-Happy_Birthday_GNU-hq_600px_780kbit.ogv" controls="control" height=200>
 			Your browser does not support video.
 		</video>
-		</br>
+		<br />
 		<script type="text/javascript">
 		var userEnteredNumber = prompt( "Please enter a number", "Your number" );
 		document.write(userEnteredNumber);
@@ -24,6 +22,8 @@
 		document.write( myResponse );
 		</script>
 		</div>
+		<div id="hoverbutton">
+			<a href=""></a>
+		</div>
 	</body>
-	</div>
 </html>
